d0dea96878927ce0a172a4ce013413a8f311e4fe
Ch0min <marklundgaard7@gmail.com>
2021-11-17 13:21:54 +0100

    UP

--- SP3/src/MainPackage/DBConnector.java
+++ SP3/src/MainPackage/DBConnector.java
@@ -1,4 +1,119 @@
 package MainPackage;
+import java.sql.*;
+import java.util.ArrayList;
 
-public class DBConnector {
+public class DBConnector implements IO {
+    static final String DB_URL = "jdbc:mysql://localhost/HoldDatabase";
+    static final String USER = "root";
+    static final String PASS = "password";
+
+    public String[] readTeamData() {
+        String[] team_data = new String[16];
+        Connection conn = null;
+        Statement stmt = null;
+        int count = 0;
+
+        try {
+            System.out.println("***Forbinder til database***");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            System.out.println("**Opretter queries**");
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM Hold";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String hold = rs.getString("hold");
+                String player1 = rs.getString("player1");
+                String player2 = rs.getString("player2");
+                boolean result = rs.getBoolean("result");
+
+                team_data[count] = hold + "," + player1 + "," + player2 + "," + result;
+                count++;
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException se) {
+            // Behandler errors for JDBC.
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Brugt til at close alle resources.
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se3) {
+                se3.printStackTrace();
+            }   // afslutter finally try.
+        }       // afslutter selve try.
+        return team_data;
+    }
+
+    public void saveGameData(ArrayList<Team> teamList) {
+        Connection conn = null;
+        String sql = "INSERT INTO Hold(hold, player1, player2, result) "
+                + "VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE hold=?, result=?";
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            PreparedStatement pstmt = conn.prepareStatement((sql, Statement.RETURN_GENERATED_KEYS);
+
+            for(int i = 0; i < teamList.size(); i++) {
+
+                Team t = teamList.get(i);
+                String player1 = t.getTeamPlayers().get(0);
+                String player2 = t.getTeamPlayers().get(1);
+
+                pstmt.setString(1, t.getTeamName());
+                pstmt.setString(2, player1);
+                pstmt.setString(3, player2);
+                pstmt.setBoolean(4, t.getHaveLost());
+
+                pstmt.addBatch();
+
+            }
+            pstmt.executeBatch();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
